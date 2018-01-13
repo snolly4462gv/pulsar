4a1f99b8b71db1fe9f01e56b394671fa73853e58
lexa <keitmark@mail.ru>
2018-01-13 13:18:26 +0300

    laaa

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,24 @@
 $(document).ready(function () {
-    
+    $('.sl-2').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        asNavFor: '.sl-1',
+        infinite: false
+    });
+    $('.sl-1').slick({
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        asNavFor: '.sl-2',
+        dots: false,
+        arrows: false,
+        focusOnSelect: true
+    });
+
+
+
+
+
+
+
 });
