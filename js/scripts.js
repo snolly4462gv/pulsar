306ee8c9d43973f5c34f04e6f0c69fd9474c657c
lexa <keitmark@mail.ru>
2018-01-20 00:02:49 +0300

    fixes almost done

--- js/scripts.js
+++ js/scripts.js
@@ -63,7 +63,7 @@ $(document).ready(function () {
     var scrFunc = function () {
         var t = win.scrollTop(),
             e = win.height();
-        $("[data-anim], .list-should-apply,.sec-lvl-4 .for-flex-cont,.sec-lvl-5 .list-acceleration .one-item, .sec-lvl-6 .max-w-960,.sec-lvl-7 .timeline .one-time,.section-11-review .max-w-960 .one-review-item,.section-12-STARTUPS .flexfour .block-one,.for-main-counter,.section-14-team .flex-five").each(function (n, i) {
+        $("[data-anim], .list-should-apply,.sec-lvl-4 .for-flex-cont,.sec-lvl-5 .list-acceleration .one-item, .sec-lvl-6 .max-w-960,.sec-lvl-7 .timeline .one-time,.section-11-review .max-w-960 .one-review-item,.section-12-STARTUPS .flexfour .block-one,.for-main-counter,.section-14-team").each(function (n, i) {
             var r = $(i).offset().top,
                 s = t + .9 * e;
             s > r ? $(i).attr("data-anim", "true") : true;
